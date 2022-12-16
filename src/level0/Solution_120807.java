23dfcc827f4c6f4674f25c43455f58882d869ff1
Pro-Lily <programmer_ls@naver.com>
2022-12-16 17:02:45 +0900

    숫자 비교하기
    https://school.programmers.co.kr/learn/courses/30/lessons/120807

---
+++ src/level0/Solution_120807.java
@@ -0,0 +1,21 @@
+package level0;
+
+public class Solution_120807 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(2, 3));
+		System.out.println(solution(11, 11));
+		System.out.println(solution(7, 99));
+	}
+
+	public static int solution(int num1, int num2) {
+		int answer = 0;
+		if (num1 == num2) {
+			answer = 1;
+		} else {
+			answer = -1;
+		}
+		return answer;
+	}
+	
+}
