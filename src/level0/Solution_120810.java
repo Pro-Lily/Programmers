d40887ab2b675e7e8876c0df7081a2f4f4433aed
Pro-Lily <programmer_ls@naver.com>
2022-12-16 17:03:47 +0900

    나머지 구하기
    https://school.programmers.co.kr/learn/courses/30/lessons/120810

---
+++ src/level0/Solution_120810.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class Solution_120810 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(3, 2));
+		System.out.println(solution(10, 5));
+	}
+
+	public static int solution(int num1, int num2) {
+		return num1 % num2;
+	}
+
+}
