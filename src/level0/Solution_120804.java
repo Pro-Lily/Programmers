964c011139f422b52e095fb6ce2ee148dad8396e
Pro-Lily <programmer_ls@naver.com>
2022-12-16 17:00:12 +0900

    두 수의 곱
    https://school.programmers.co.kr/learn/courses/30/lessons/120804

---
+++ src/level0/Solution_120804.java
@@ -0,0 +1,11 @@
+package level0;
+
+public class Solution_120804 {
+	public static void main(String[] args) {
+		System.out.println(solution(3, 4));
+		System.out.println(solution(27, 19));
+	}
+	public static int solution(int num1, int num2) {
+		return num1 * num2;
+	}
+}
