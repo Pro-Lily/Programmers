0a88499607f794ede3412e63639ca1f8f55b4b2e
Pro-Lily <programmer_ls@naver.com>
2022-12-16 17:04:47 +0900

    두 수의 합
    https://school.programmers.co.kr/learn/courses/30/lessons/120802

---
+++ src/level0/Solution_120802.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class Solution_120802 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(2, 3));
+		System.out.println(solution(100, 2));
+	}
+
+	public static int solution(int num1, int num2) {
+		return num1 + num2;
+	}
+
+}
