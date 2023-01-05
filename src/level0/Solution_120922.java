11b14d82f9434284eb3141b9f3acd830c2916c3f
Pro-Lily <programmer_ls@naver.com>
2023-01-05 16:22:49 +0900

    종이 자르기
    https://school.programmers.co.kr/learn/courses/30/lessons/120922

---
+++ src/level0/Solution_120922.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class Solution_120922 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(2, 2));
+		System.out.println(solution(2, 5));
+		System.out.println(solution(1, 1));
+	}
+
+	public static int solution(int M, int N) {
+		return M * N - 1;
+	}
+}
