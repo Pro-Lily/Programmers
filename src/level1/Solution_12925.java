845a19121172b00c80719eb7f31ab718c03a210d
Pro-Lily <programmer_ls@naver.com>
2023-01-26 17:39:38 +0900

    문자열을 정수로 바꾸기
    https://school.programmers.co.kr/learn/courses/30/lessons/12925

---
+++ src/level1/Solution_12925.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class Solution_12925 {
+
+	public static void main(String[] args) {
+		System.out.println(solution("1234"));
+		System.out.println(solution("-1234"));
+	}
+
+	public static int solution(String s) {
+		return Integer.parseInt(s);
+	}
+
+}
